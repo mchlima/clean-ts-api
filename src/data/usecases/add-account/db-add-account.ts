53634c1fc392f8535817a9f36d049def5a2eea77
Michel Lima <michel.lima@pagtel.com.br>
2023-05-30 08:55:27 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -5,11 +5,11 @@ import type { Encrypter } from '../../protocols/encrypter'
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
 
-  constructor(encrypter: Encrypter) {
+  constructor (encrypter: Encrypter) {
     this.encrypter = encrypter
   }
 
-  async add(account: AddAccountModel): Promise<AccountModel> {
+  async add (account: AddAccountModel): Promise<AccountModel> {
     await this.encrypter.encrypt(account.password)
     return new Promise(resolve => { resolve(null) })
   }
