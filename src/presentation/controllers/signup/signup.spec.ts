1a01a941b5cf037d63e031b9bdc679c20c07f683
Michel Lima <michel.lima@pagtel.com.br>
2023-05-31 11:15:06 -0300

    refactor: return the error stack

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -166,7 +166,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('an_error'))
   })
 
   test('Should return 500 if AddAccount throws', async () => {
@@ -186,7 +186,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('an_error'))
   })
 
   test('Should call AddAccount with correct values', async () => {
