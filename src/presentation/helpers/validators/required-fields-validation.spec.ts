09f65df0cd3f4a77ddc58fc4e41b35d5546d8416
Michel Lima <m.macedomarques@gmail.com>
2023-06-01 14:47:35 -0300

    test: ensure

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredFieldValidation', () => {
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
+
+  test('should not return a MissinParamError if validation success', () => {
+    const sut = new RequiredFieldsValidation('any_field')
+    const error = sut.validate({ any_field: 'any_name' })
+    expect(error).toBeFalsy()
+  })
 })
