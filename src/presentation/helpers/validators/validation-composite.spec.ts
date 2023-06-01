ddd9160a6e8089f9ad429f729db327c4c14cc107
Michel Lima <m.macedomarques@gmail.com>
2023-06-01 15:11:17 -0300

    refactor: factory

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,35 @@
+import { MissingParamError } from '../../errors'
+import { type Validation } from './validation'
+import { ValidationComposite } from './validation-composite'
+
+const makeValidationStub = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    sut,
+    validationStub
+  }
+}
+
+describe('ValidationComposite', () => {
+  test('should return an error if any validation fails', () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+})
--- src/presentation/helpers/validators/validation-composite.ts
+++ src/presentation/helpers/validators/validation-composite.ts
@@ -7,7 +7,7 @@ export class ValidationComposite implements Validation {
     this.validations = validations
   }
 
-  validate (input: Validation[]): Error {
+  validate (input: any): Error {
     for (const validation of this.validations) {
       const error: Error = validation.validate(input)
       if (error) {
