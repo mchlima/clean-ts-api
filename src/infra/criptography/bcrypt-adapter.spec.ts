cefb810190cba36ba94520c35f2fb649787fdf8d
Michel Lima <m.macedomarques@gmail.com>
2023-06-15 12:26:08 -0300

    feat: ensure BcryptAdapter return true when bcrypt compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -49,4 +49,10 @@ describe('Bcrypt Adapter', () => {
     await sut.compare('any_value', 'any_hash')
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
+
+  test('should return true then compare succeeds', async () => {
+    const sut = makeSut()
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(true)
+  })
 })
