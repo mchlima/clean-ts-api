4f9c002173b2c13d157b666cf1c03ed481246110
Michel Lima <m.macedomarques@gmail.com>
2023-06-15 12:40:25 -0300

    test: should BcryptAdapter throw if becrypt compare throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -33,9 +33,6 @@ describe('Bcrypt Adapter', () => {
 
   test('should thow if bcrypt thows', async () => {
     const sut = makeSut()
-    // jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((_resolve, reject) => { reject(new Error()) }))
-
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     jest.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => {
       return new Promise((_resolve, reject) => { reject(new Error()) })
     })
@@ -63,5 +60,14 @@ describe('Bcrypt Adapter', () => {
     })
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(false)
+  })
+
+  test('should throw if bcrypt compare throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => {
+      return new Promise((_resolve, reject) => { reject(new Error()) })
+    })
+    const promise = sut.compare('any_value', 'any_hash')
+    await expect(promise).rejects.toThrow()
   })
 })
